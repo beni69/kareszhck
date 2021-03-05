5ac838d4c9676e9eae989688ad48c16bc6d565b1
beni69 <73666440+beni69@users.noreply.github.com>
2021-03-05 17:27:31 +0100

    complete redesign
    unlocking items is on the way.
    now with autoload

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,20 @@
+// get the current tab id
+let tabId;
+chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    tabId = tabs[0].id;
+
+    chrome.tabs.sendMessage(tabId, "ping", res => {
+        // set up content script if it doesnt exist
+        if (chrome.runtime.lastError || res != "pong")
+            chrome.scripting.executeScript({
+                target: { tabId },
+                function: content,
+            });
+        console.log(res);
+    });
+});
+
+// stuff on submit
 document.getElementById("form").addEventListener("submit", e => {
     e.preventDefault();
 
@@ -5,5 +22,42 @@ document.getElementById("form").addEventListener("submit", e => {
     const kps = document.getElementById("kps").value;
     const kpc = document.getElementById("kpc").value;
 
-    console.log({ k, kps, kpc });
+    const opts = { k, kps, kpc };
+
+    chrome.tabs.sendMessage(tabId, opts);
 });
+
+document.getElementById("resetBtn").addEventListener("click", e => {
+    const opts = { k: "0", kps: "0", kpc: "0", reset: true };
+
+    chrome.tabs.sendMessage(tabId, opts);
+});
+
+// actual content script
+function content() {
+    chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+        if (msg == "ping") return sendResponse("pong");
+
+        console.debug({ msg, sender });
+
+        const warning =
+            "WARNING: This will overwrite your save and your current game.\nDo you want to continue?";
+        if (!confirm(warning)) return;
+
+        const { k, kps, kpc } = msg;
+        if (k != "") localStorage.setItem("kareszCount", k);
+        if (kps != "") localStorage.setItem("kps", kps);
+        if (kpc != "") localStorage.setItem("kpc", kpc);
+
+        if (!localStorage.getItem("hackTip")) {
+            localStorage.setItem("hackTip", true);
+            const hackTip = `Done. We will try to automatically load the changes, but if that doesn't work, click the "Load" button.\n\nThis message will only show once`;
+            alert(hackTip);
+        }
+
+        const loadBtn = document.getElementById("loadButton");
+        loadBtn.click();
+    });
+
+    console.log("Karesz Clicker Hack > Content script loaded!");
+}
